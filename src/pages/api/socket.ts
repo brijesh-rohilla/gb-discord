a89f628911264b43aec638b42347a1390d0b3fb9
Brijesh Rohilla <66164179+brijesh-rohilla@users.noreply.github.com>
2023-10-18 17:02:13 +0530

    feat: chat now save to database (#5)

--- src/pages/api/socket.ts
+++ src/pages/api/socket.ts
@@ -2,6 +2,10 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { Socket } from 'socket.io';
 import CustomSocketServer from '@/types/extended/custom-socket-server';
 
+import { MessageType } from '@/types';
+import connectSheet from '../../../utils/connectSheet';
+import { getSheetInstance } from '../../../utils/sheet-helpers';
+
 export default function SocketHandler(
   _req: NextApiRequest,
   res: NextApiResponse<any>,
@@ -24,9 +28,20 @@ export default function SocketHandler(
   socketUntyped.server.io = io;
 
   io.on('connection', (socket: Socket) => {
-    socket.on('send-message', (obj) => {
-      console.log(`message: ${JSON.stringify(obj)}`);
-      socket.broadcast.emit('receive-message', obj);
+    socket.on('send-message', async (msg: MessageType) => {
+      const doc = await connectSheet();
+      const sheet = getSheetInstance({ doc, method: 'sheetsByTitle', key: 'chat' });
+
+      sheet.addRow({
+        timestamp: new Date(),
+        user_id: msg.userId,
+        message: msg.message,
+        user_name: msg.userName,
+        message_id: msg.messageId,
+      });
+
+      console.log(`message: ${JSON.stringify(msg)}`);
+      socket.broadcast.emit('receive-message', msg);
     });
   });
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import dynamic from 'next/dynamic';
 import { v4 as uuidv4 } from 'uuid';
 import io, { Socket } from 'socket.io-client';
 import { MessageType, UserType, userDefault } from '@/types';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import useLocalStorage from '../../@hooks/useLocalStorage';
 
 const UserLoginModal = dynamic(() => import('@/components/user-login-modal'), { ssr: false });
@@ -12,16 +13,32 @@ let socket: Socket;
 
 const HomePage = () => {
   const [message, setMessage] = useState('');
+  const [hasMore, setHasMore] = useState(true);
+  const [msgOffset, setMsgOffset] = useState(10);
+
   const [user] = useLocalStorage<UserType>('user', userDefault);
   const [messagesList, setMessagesList] = useState<MessageType[]>([]);
 
   async function socketInitializer() {
+    const data = await fetch('/api/sheet-action?limit=10&offset=0');
+    const messageData = await data.json();
+
+    setMessagesList([
+      ...messageData.map((x: any) => ({
+        message: x.data.message,
+        userId: x.data.user_id,
+        userName: x.data.user_name,
+        timestamp: x.data.timestamp,
+        messageId: x.data.message_id,
+      })),
+    ]);
+
     await fetch('/api/socket?EIO=4&transport=polling');
     socket = io({ path: '/api/socket' });
 
     socket.on('receive-message', (msg: MessageType) => {
       console.log('receive-message', msg);
-      setMessagesList((pre) => [...pre, msg]);
+      setMessagesList((pre) => [msg, ...pre]);
       window.scrollTo(0, document.body.scrollHeight);
     });
   }
@@ -47,8 +64,31 @@ const HomePage = () => {
     };
 
     socket.emit('send-message', msg);
-    setMessagesList((pre) => [...pre, msg]);
+    setMessagesList((pre) => [msg, ...pre]);
     setMessage('');
+  };
+
+  const fetchData = async () => {
+    const data = await fetch(`/api/sheet-action?limit=10&offset=${msgOffset}`);
+    const messageData = await data.json();
+    setMsgOffset((prev) => prev + 10);
+
+    if (messageData.length < 10) {
+      setHasMore(false);
+    }
+
+    setMessagesList((pre) => [
+      ...pre,
+      ...messageData.map((x: any) => ({
+        message: x.data.message,
+        userId: x.data.user_id,
+        userName: x.data.user_name,
+        timestamp: x.data.timestamp,
+        messageId: x.data.message_id,
+      })),
+    ]);
+
+    return messageData;
   };
 
   return (
@@ -57,23 +97,44 @@ const HomePage = () => {
         <div className='row'>
           <div className='col'>
 
-            <ul>
-              {messagesList.map((data, index) => {
-                const msgAlignClass = data.userId === user.userId ? 'text-right' : 'text-left';
-                const msgColorClass = data.userId === user.userId ? 'bg-primary' : 'bg-secondary';
+            <ul className='mb-5 vh-100'
+              id="msg-box"
+              style={{
+                overflow: 'auto',
+                display: 'flex',
+                flexDirection: 'column-reverse',
+              }}
+            >
+              <InfiniteScroll
+                inverse={true}
+                next={fetchData}
+                hasMore={hasMore}
+                dataLength={messagesList.length}
+                style={{ display: 'flex', flexDirection: 'column-reverse' }}
+                scrollableTarget="msg-box"
+                loader={<h4>Loading...</h4>}
+                endMessage={
+                  <p style={{ textAlign: 'center' }}>
+                    <b>Yay! You have seen it all</b>
+                  </p>
+                }>
+                {messagesList.map((data, index) => {
+                  const msgAlignClass = data.userId === user.userId ? 'text-right' : 'text-left';
+                  const msgColorClass = data.userId === user.userId ? 'bg-primary' : 'bg-secondary';
 
-                return (
-                  <li key={index} className={`py-1 ${msgAlignClass}`}>
-                    <div className={`d-inline-block px-3 py-1 text-white rounded ${msgColorClass}`}>
-                      <div className={`small mb-1 text-light font-weight-bold ${msgAlignClass}`}>{data.userName}</div>
-                      <div>{data.message}</div>
-                      <div className='small text-right'>
-                        {`${new Date(data.timestamp).getHours()} : ${new Date(data.timestamp).getMinutes()}`}
+                  return (
+                    <li key={index} className={`py-1 ${msgAlignClass}`}>
+                      <div className={`d-inline-block px-3 py-1 text-white rounded ${msgColorClass}`}>
+                        <div className={`small mb-1 text-light font-weight-bold ${msgAlignClass}`}>{data.userName}</div>
+                        <div>{data.message}</div>
+                        <div className='small text-right'>
+                          {`${new Date(data.timestamp).getHours()} : ${new Date(data.timestamp).getMinutes()}`}
+                        </div>
                       </div>
-                    </div>
-                  </li>
-                );
-              })}
+                    </li>
+                  );
+                })}
+              </InfiniteScroll>
             </ul>
 
             <form id="form" onSubmit={handleSubmit}>
